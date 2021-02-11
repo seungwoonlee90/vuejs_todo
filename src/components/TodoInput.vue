83d31e0d15236d8b54d5dc1838fef421d9b3a05b
seungwoonlee90 <seungwoonlee90@gmail.com>
2021-02-11 20:53:09 +0900

    add modal & transition

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,33 @@
       <span class="addContainer" v-on:click="addTodo">
         <i class="fas fa-plus addBtn"></i>
       </span>
+
+      <Modal v-if="showModal" @close="showModal = false">
+    <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+        <template v-slot:header>
+          <h3>warning!!
+            <i class="closeModalBtn fas fa-times" @click="showModal = false"></i>
+          </h3>
+        </template>
+        <template v-slot:body>
+          <h3>무언가를 입력하세요.</h3>
+        </template>
+      </Modal>
   </div>
 </template>
 
 <script>
+
+import Modal from './common/Modal.vue'
+
 export default {
   data: function() {
     return {
-      newTodoItem:""
+      newTodoItem:"",
+      showModal:false
     }
   },
   methods: {
@@ -19,11 +38,16 @@ export default {
       if (this.newTodoItem !== '') {
         this.$emit('addTodoItem',this.newTodoItem);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function() {
       this.newTodoItem='';
     }
+  },
+  components: {
+    Modal:Modal
   }
 }
 </script>
@@ -53,5 +77,8 @@ input:focus {
   color: white;
   vertical-align: middle;
 }
+.closeModalBtn {
+  color: #42B983;
 
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <ul>
+      <transition-group name="list" tag="ul">
         <li v-for= "(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
           <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
           <span v-bind:class="{textCompleted : todoItem.completed}">{{ todoItem.item }}</span>
@@ -8,7 +8,7 @@
             <i class="fas fa-trash"></i>
           </span>
         </li>
-      </ul>
+      </transition-group>
   </div>
 </template>
 
@@ -60,5 +60,14 @@ li {
   color: #b3adad;
 }
 
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 
 </style>
